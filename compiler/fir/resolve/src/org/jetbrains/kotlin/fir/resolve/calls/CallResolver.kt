e70418b4fc3ea7200922e30bcaf70b9615b11459
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-04-16 12:07:37 +0300

    Collect statistics about overload conflict / get applicability time

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/CallResolver.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/calls/CallResolver.kt
@@ -15,6 +15,7 @@ import org.jetbrains.kotlin.fir.resolve.FirSymbolProvider
 import org.jetbrains.kotlin.fir.resolve.ScopeSession
 import org.jetbrains.kotlin.fir.resolve.scope
 import org.jetbrains.kotlin.fir.resolve.substitution.ConeSubstitutor
+import org.jetbrains.kotlin.fir.resolve.transformers.FirBodyResolveTransformer
 import org.jetbrains.kotlin.fir.resolve.transformers.ReturnTypeCalculator
 import org.jetbrains.kotlin.fir.resolve.transformers.ReturnTypeCalculatorWithJump
 import org.jetbrains.kotlin.fir.scopes.FirPosition
@@ -32,6 +33,7 @@ import org.jetbrains.kotlin.resolve.calls.inference.model.ConstraintStorage
 import org.jetbrains.kotlin.resolve.calls.model.PostponedResolvedAtomMarker
 import org.jetbrains.kotlin.resolve.calls.tasks.ExplicitReceiverKind
 import org.jetbrains.kotlin.utils.addToStdlib.cast
+import kotlin.system.measureNanoTime
 
 class CallInfo(
     val callKind: CallKind,
@@ -538,7 +540,10 @@ class CandidateCollector(val callInfo: CallInfo) {
     }
 
     fun consumeCandidate(group: Int, candidate: Candidate) {
-        val applicability = getApplicability(group, candidate)
+        lateinit var applicability: CandidateApplicability
+        FirBodyResolveTransformer.getApplicabilityTime += measureNanoTime {
+            applicability = getApplicability(group, candidate)
+        }
 
         if (applicability > currentApplicability) {
             groupNumbers.clear()
