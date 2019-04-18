bc275d7b6d094fb732664509bcb42caa7812d84f
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-04-18 17:13:17 +0300

    Raw FIR: synthesize componentX in data classes

---
+++ compiler/fir/resolve/testData/resolve/expresssions/components.kt
@@ -0,0 +1,10 @@
+data class D(val x: Int, val y: String)
+
+fun foo(list: List<D>) {
+    for ((x, y) in list) {}
+    val (x, y) = list.first()
+    list.forEach { (x, y) ->
+        println(x)
+        println(y)
+    }
+}
